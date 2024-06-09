4bd9c1723bfda39727aafb318e41f105a3c935ee
James Kanin <kaninnoothep@gmail.com>
2024-06-09 14:49:34 -0600

    fix typo

--- client/src/services/api/course/useCreateCourseMutation.js
+++ client/src/services/api/course/useCreateCourseMutation.js
@@ -2,12 +2,12 @@ import { useMutation } from "@tanstack/react-query";
 import { CREATE_COURSE_API_KEY } from "services/constants";
 import { apiRequest } from "services/helpers/apiRequest";
 
-const createCoursRequest = (payload) =>
+const createCourseRequest = (payload) =>
   apiRequest(CREATE_COURSE_API_KEY, "POST", payload);
 
 export const useCreateCourseMutation = (options) =>
   useMutation({
     mutationKey: [CREATE_COURSE_API_KEY],
-    mutationFn: createCoursRequest,
+    mutationFn: createCourseRequest,
     ...options,
   });
